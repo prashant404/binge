415f5a4ff87ea4a836d7a299d1263dc88eb1e0d4
Prashant Kumar <pkr214204@gmail.com>
2024-04-30 19:28:09 +0530

    changes

--- bingeBox/src/utils/constant.js
+++ bingeBox/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const API_END_POINT = "http://localhost:8080/api/v1/user";
+export const API_END_POINT = "https://binge-vlfi.onrender.com/api/v1/user";
 
 export const options = {
     method: 'GET',
