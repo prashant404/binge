d596f419cbc1000521466dbb92f3036f6ae7e028
Prashant Kumar <pkr214204@gmail.com>
2024-04-30 08:13:56 +0530

    added

---
+++ bingeBox/src/components/VideoTitle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VideoTitle = ({ title, overview }) => {
+  return <div className="w-[vw] absolute text-white pt-[18%] p-12"></div>;
+};
+
+export default VideoTitle;
